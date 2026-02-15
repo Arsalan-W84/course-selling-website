688d491b081bc550bcee1c609f3af8960f09257a
Arsalan-W84 <arsalanwahid0804@gmail.com>
2026-02-15 11:17:57 +0000

    middlewares logic defined and gitignore added

--- auth/adminmiddleware.js
+++ auth/adminmiddleware.js
@@ -1,7 +1,31 @@
+const jwt = require("jsonwebtoken");
+const ADMIN_SECRET = "UGEOIC56YTUA74VBNNRUMJC";
+
 function admin_auth(req,res,next)   
 {
-    next();
+   const token = req.headers.token;
+    if(token){//token found , now verify it 
+        const founduser = jwt.verify(token , ADMIN_SECRET);
+        if(founduser){
+            req.userId = founduser.id;
+            next();
+        }else{
+            return res.json(403).json({
+                message : "User not signed in!"
+            });
+        }
+    }else{//token wasn't found
+        return res.status(403).json({
+            message : "Unauthorized!"
+        });
+    }
 }
+
+
+
+module.exports = {
+    user_auth : user_auth
+};
 
 module.exports = {
     admin_auth : admin_auth
--- auth/usermiddleware.js
+++ auth/usermiddleware.js
@@ -1,7 +1,24 @@
+const jwt = require("jsonwebtoken");
+const USER_SECRET = "CERYTUANUMJOIUG5674VBNC";
 function user_auth(req,res,next)
-{
-    //verify the token sent 
-    next();
+{ 
+    const token = req.headers.token;
+    if(token){//token found , now verify it 
+        const founduser = jwt.verify(token , USER_SECRET);
+        if(founduser){
+            req.userId = founduser.id;
+            next();
+        }else{
+            return res.json(403).json({
+                message : "User not signed in!"
+            });
+        }
+    }else{//token wasn't found
+        return res.status(403).json({
+            message : "Unauthorized!"
+        });
+    }
+    
 }
 
 module.exports = {
